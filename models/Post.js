cf178fd8e8549ae0696dd6dcfbb55333fedc195a
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-03-24 20:36:50 -0400

    quick refactor on Post.js

--- models/Post.js
+++ models/Post.js
@@ -2,30 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize =  require('../config/connection');
 
 // create our Post model
-class Post extends Model {
-    static upVote(body, models) {
-        return models.Vote.create({
-            user_id: body.user_id,
-            post_id: body.post_id
-        }).then(() => {
-            return Post.findOne({
-                where: {
-                    id: body.post_id
-                },
-                attributes: [
-                    'id',
-                    'post_url',
-                    'title',
-                    'created_at',
-                    [
-                        sequelize.literal('(SELECT COUNT(*) FROM bote WHERE post.id = vote.post_id)'),
-                        'vote_count'
-                    ]
-                ]
-            });
-        });
-    }
-}
+class Post extends Model {}
 
 // create fields/columns for Post model
 // user_id is the foreign key and will reference the User model
