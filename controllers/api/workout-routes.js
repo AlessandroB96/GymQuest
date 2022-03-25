dce67e37d3bb66d42ca1cf7d8ddd265fe3d60768
AlessandroB96 <abelaj7196@gmail.com>
2022-03-25 19:33:38 -0400

    added individual workout get route needed for each page

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -22,4 +22,27 @@ router.get('/workouts', (req, res) => {
     });
 });
 
+router.get('/workouts/:workout_name', (req, res) => {
+    Workout.findOne({
+        where: {
+            workout_name: req.params.workout_name
+        },
+        attributes: [
+        'id',
+        'workout_name',
+    ],
+        include: [
+            {
+                model: Category,
+                attributes: ['id','category_name']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
