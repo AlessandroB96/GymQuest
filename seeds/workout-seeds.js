f838364b5b3d5c5f08eeba93e2325873b45950c1
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-03-24 20:19:31 -0400

    adding files to develop

--- seeds/workout-seeds.js
+++ seeds/workout-seeds.js
@@ -3,27 +3,22 @@ const { Workout } = require('../models');
 const workoutData = [
   {
     workout_name: 'Skullcrushers',
-    muscle_group: 'Triceps',
     category_id: 1,
   },
   {
     workout_name: 'Leg press',
-    muscle_group: 'Quads, calvs',
     category_id: 5,
   },
   {
     workout_name: 'Bicep curls',
-    muscle_group: 'Biceps',
     category_id: 4,
   },
   {
     workout_name: 'Seated rows',
-    muscle_group: 'Back',
     category_id: 3,
   },
   {
     workout_name: 'Cardio',
-    muscle_group: 'Full-body',
     category_id: 2,
   },
 ];
