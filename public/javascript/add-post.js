5f6c9b110ea5b26ca4e85c5a3f54bd92e6db9d0f
ajmarrocco <ajmarrocco@gmail.com>
2022-03-26 15:17:44 -0400

    update add post js to have the correct body

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,14 +1,14 @@
 async function newFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_url = document.querySelector('input[name="post-url"]').value;
+    const workout_name = document.querySelector('input[name="post-title"]').value;
+    const workout_url = document.querySelector('input[name="post-url"]').value;
 
-    const response = await fetch(`/api/posts`, {
+    const response = await fetch(`/api/workouts`, {
         method: 'POST',
         body: JSON.stringify({
-            title,
-            post_url
+            workout_name,
+            workout_url
         }),
         headers: {
             'Content-Type': 'application/json'
