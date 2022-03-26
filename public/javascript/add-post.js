973d4bf0b73e700faf7d4eebf8b6134c3b4883e9
ajmarrocco <ajmarrocco@gmail.com>
2022-03-25 20:31:20 -0400

    login logout signup public js files

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,7 +2,7 @@ async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_content = document.querySelector('input[name="post-url"]').value;
+    const post_url = document.querySelector('input[name="post-url"]').value;
 
     const response = await fetch(`/api/posts`, {
         method: 'POST',
@@ -16,7 +16,7 @@ async function newFormHandler(event) {
     });
 
     if (response.ok) {
-        document.location.replace('/dashboard');
+        document.location.reload();
     } else {
         alert(response.statusText);
     }
